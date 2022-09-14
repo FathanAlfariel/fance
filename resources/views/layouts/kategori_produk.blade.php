6694e7fba8068c4839afd42c89b30a34030663a9
FathanAlfariel <faril191204@gmail.com>
2022-09-14 20:22:53 +0700

    first commit
    new commit

--- resources/views/layouts/kategori_produk.blade.php
+++ resources/views/layouts/kategori_produk.blade.php
@@ -1,78 +1,62 @@
-<h3>Kategori Produk</h3>
-<div class="text-center">
-    <div class="row">
-        <div class="col">
-            <div class="p-3 border bg-light">
-                <div class="row g-2">
-                    <div class="col-6">
-                        <div class="p-3 border bg-light">Custom column padding</div>
-                    </div>
-                    <div class="col-6">
-                        <div class="p-3 border bg-light">Custom column padding</div>
-                    </div>
-                    <div class="col-6">
-                        <div class="p-3 border bg-light">Custom column padding</div>
-                    </div>
-                    <div class="col-6">
-                        <div class="p-3 border bg-light">Custom column padding</div>
-                    </div>
-                </div>
-            </div>
+<h3 class="fw-bolder ps-3 pt-3">Kategori Produk</h3>
+<div class="row row-cols-1 row-cols-md-4 g-3 w-100 ps-3">
+    <div class="col">
+      <div class="card h-100 rounded-0">
+        <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp" class="" alt="Skyscrapers"/>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in to
+            additional content. This content is a little bit longer.
+          </p>
         </div>
-        <div class="col">
-            <div class="p-3 border bg-light">
-                <div class="row g-2">
-                    <div class="col-6">
-                        <div class="p-3 border bg-light">Custom column padding</div>
-                    </div>
-                    <div class="col-6">
-                        <div class="p-3 border bg-light">Custom column padding</div>
-                    </div>
-                    <div class="col-6">
-                        <div class="p-3 border bg-light">Custom column padding</div>
-                    </div>
-                    <div class="col-6">
-                        <div class="p-3 border bg-light">Custom column padding</div>
-                    </div>
-                </div>
-            </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
         </div>
-        <div class="col">
-            <div class="p-3 border bg-light">
-                <div class="row g-2">
-                    <div class="col-6">
-                        <div class="p-3 border bg-light">Custom column padding</div>
-                    </div>
-                    <div class="col-6">
-                        <div class="p-3 border bg-light">Custom column padding</div>
-                    </div>
-                    <div class="col-6">
-                        <div class="p-3 border bg-light">Custom column padding</div>
-                    </div>
-                    <div class="col-6">
-                        <div class="p-3 border bg-light">Custom column padding</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="p-3 border bg-light">
-                <div class="row g-2">
-                    <div class="col-6">
-                        <div class="p-3 border bg-light">Custom column padding</div>
-                    </div>
-                    <div class="col-6">
-                        <div class="p-3 border bg-light">Custom column padding</div>
-                    </div>
-                    <div class="col-6">
-                        <div class="p-3 border bg-light">Custom column padding</div>
-                    </div>
-                    <div class="col-6">
-                        <div class="p-3 border bg-light">Custom column padding</div>
-                    </div>
-                </div>
-                <a href="">See more</a>
-            </div>
-        </div>
+      </div>
     </div>
+    <div class="col">
+      <div class="card h-100 rounded-0">
+        <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp" class="" alt="Los Angeles Skyscrapers"/>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100 rounded-0">
+        <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" class="" alt="Palm Springs Road"/>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in to
+            additional content. This card has even longer content than the first to show
+            that equal height action.
+          </p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+        <div class="card h-100 rounded-0">
+          <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" class="" alt="Palm Springs Road"/>
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural lead-in to
+              additional content. This card has even longer content than the first to show
+              that equal height action.
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+      </div>
   </div>
